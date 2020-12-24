749b7e35c9b437f6c9972e3b0e58c760b92ffcf6
MahmoudIbrahim1012 <mahmoud20191700591@cis.asu.edu.eg>
2020-12-24 21:17:30 +0200

    main

--- src/com/project/Main.java
+++ src/com/project/Main.java
@@ -1,8 +1,27 @@
 package com.project;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
+        Scanner input = new Scanner(System.in);
+        System.out.printf("""
+                ---------- CAR SALES SYSTEM -----------
+                1 - SIGN IN
+                2 - SIGN UP\s""");
+        int choice = input.nextInt();
+        switch (choice) {
+            case 1:
+                //sign in function
+                break;
+            case 2:
+                // sign up function
+                break;
+            default:
+                System.out.println("Invalid input. ");
+        }
+
+
     }
 }
